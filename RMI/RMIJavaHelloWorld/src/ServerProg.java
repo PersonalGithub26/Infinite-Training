476432b460282742b337eed94ac68ad77e3c3106
chandud <chandud@172.23.7.79>
2023-01-30 11:53:58 +0530

    New things

---
+++ RMI/RMIJavaHelloWorld/src/ServerProg.java
@@ -0,0 +1,24 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+
+public class ServerProg {
+	
+	public static void main(String[] args) {
+		System.setSecurityManager(new RMISecurityManager());
+		try {
+			Hello obj = new HelloImpl();
+			Naming.rebind("localhost", obj);
+			System.out.println("Server is ready........."+"\n"+" Waiting for Client request");
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
